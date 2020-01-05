91e77122ae159b682739a19e885fe10a502f49f3
Owen Burns <owenburns88@gmail.com>
2020-01-05 12:54:21 -0500

    commit 1

--- lowdataYT_APP/lowdataYT_APP/Views/PlayerPage.xaml.cs
+++ lowdataYT_APP/lowdataYT_APP/Views/PlayerPage.xaml.cs
@@ -1,10 +1,13 @@
-﻿using Newtonsoft.Json;
+﻿using NAudio.Wave;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
@@ -15,7 +18,22 @@ namespace lowdataYT_APP.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class PlayerPage : ContentPage
     {
+        private BufferedWaveProvider bufferedWaveProvider;
+        private IWavePlayer waveOut;
+        private volatile StreamingPlaybackState playbackState;
+        private volatile bool fullyDownloaded;
+        private HttpWebRequest webRequest;
+        private VolumeWaveProvider16 volumeProvider;
+
         static HttpClient client = new HttpClient();
+
+        enum StreamingPlaybackState
+        {
+            Stopped,
+            Playing,
+            Buffering,
+            Paused
+        }
 
         public PlayerPage()
         {
@@ -35,13 +53,96 @@ namespace lowdataYT_APP.Views
             foreach (Models.SearchResult x in list)
             {
                 Button resultbutton = new Button();
-                resultbutton.Clicked += (sender, e) => Result_Button_Clicked(sender, e, x.vidid);
+                resultbutton.Clicked += (sender, e) => Result_Button_Clicked(sender, e, x.vidid, x.title);
                 resultbutton.Text = x.title;
                 stack.Children.Add(resultbutton);
             }
         }
-        private void Result_Button_Clicked(object sender, EventArgs e, String id)
+        private void Result_Button_Clicked(object sender, EventArgs e, String id, String title)
         {
+            fullyDownloaded = false;
+            webRequest = (HttpWebRequest)WebRequest.Create("http://lowdatayt.ddns.net:8000/api/search/"+id+", '"+title+"'");
+            //test with definitively working mp3 stream
+            //webRequest = (HttpWebRequest)WebRequest.Create("http://listen.openstream.co/3162/audio");
+            HttpWebResponse resp;
+            try
+            {
+                resp = (HttpWebResponse)webRequest.GetResponse();
+            }
+            catch (WebException x)
+            {
+                if (x.Status != WebExceptionStatus.RequestCanceled)
+                {
+                    Console.WriteLine(x);
+                }
+                return;
+            }
+            var buffer = new byte[16384 * 4];
+            IMp3FrameDecompressor decompressor = null;
+            try{
+                using (var responsestream = resp.GetResponseStream())
+                {
+                    var readFullyStream = new Services.ReadFullyStream(responsestream);
+                    do
+                    {
+                        if (IsBufferNearlyFull)
+                        {
+                            Console.WriteLine("buffer getting full");
+                            Thread.Sleep(500);
+                        }
+                        else
+                        {
+                            Mp3Frame frame;
+                            try
+                            {
+                                frame = Mp3Frame.LoadFromStream(readFullyStream);
+                            }
+                            catch (EndOfStreamException)
+                            {
+                                fullyDownloaded = true;
+                                break;
+                            }
+                            catch (WebException)
+                            {
+                                break;
+                            }
+                            if (frame == null) break;
+                            if (decompressor == null)
+                            {
+                                decompressor = CreateFrameDecompressor(frame);
+                                bufferedWaveProvider = new BufferedWaveProvider(decompressor.OutputFormat);
+                                bufferedWaveProvider.BufferDuration = TimeSpan.FromSeconds(20);
+                            }
+                            int decompressed = decompressor.DecompressFrame(frame, buffer, 0);
+                            bufferedWaveProvider.AddSamples(buffer, 0, decompressed);
+                        }
+                    } while (playbackState != StreamingPlaybackState.Stopped);
+                        Console.WriteLine("exiting");
+                        decompressor.Dispose();
+                }
+            }
+            finally
+            {
+                if (decompressor != null)
+                {
+                    decompressor.Dispose();
+                }
+            }
+        }
+        private bool IsBufferNearlyFull
+        {
+            get
+            {
+                return bufferedWaveProvider != null &&
+                       bufferedWaveProvider.BufferLength - bufferedWaveProvider.BufferedBytes
+                       < bufferedWaveProvider.WaveFormat.AverageBytesPerSecond / 4;
+            }
+        }
+        private static IMp3FrameDecompressor CreateFrameDecompressor(Mp3Frame frame)
+        {
+            WaveFormat waveFormat = new Mp3WaveFormat(frame.SampleRate, frame.ChannelMode == ChannelMode.Mono ? 1 : 2,
+                frame.FrameLength, frame.BitRate);
+            return new AcmMp3FrameDecompressor(waveFormat);
         }
     }
 }
